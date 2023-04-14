2b0b436da4e123d64aa00ce048ea36a4dbda81a5
Sourcery AI <>
2023-04-14 15:27:39 +0000

    'Refactored by Sourcery'

--- aws-iam-parser.py
+++ aws-iam-parser.py
@@ -6,6 +6,6 @@ import json
 # Get these from curl -s http://169.254.169.254/latest/meta-data/iam/security-credentials/<profile_name>
 iam_security_creds = json.loads(sys.stdin.read())
 
-print("export AWS_ACCESS_KEY_ID=%s" % iam_security_creds['AccessKeyId'])
-print("export AWS_SECRET_ACCESS_KEY=%s" % iam_security_creds['SecretAccessKey'])
-print("export AWS_SESSION_TOKEN=%s" % iam_security_creds['Token'])
+print(f"export AWS_ACCESS_KEY_ID={iam_security_creds['AccessKeyId']}")
+print(f"export AWS_SECRET_ACCESS_KEY={iam_security_creds['SecretAccessKey']}")
+print(f"export AWS_SESSION_TOKEN={iam_security_creds['Token']}")
